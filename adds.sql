07682254f6d8577b0e11ae6bdd9a500f42b2b1d8
AbdallahMahmoudAbdElHafez <abdallah.mahmoud.te@gmail.com>
2025-12-31 15:34:51 +0200

    31.12.2025

---
+++ adds.sql
@@ -0,0 +1,12 @@
+
+
+use nurivina;
+INSERT INTO reference_types
+(id, code, label, description, created_at, updated_at)
+VALUES
+(15, 'sales_invoice_cost', 'تكلفة بضاعة مباعة', 'COGS Journal Entry for Sales Invoice', NOW(), NOW());
+use nurivina_erp;
+INSERT INTO reference_types
+(id, code, label, description, created_at, updated_at)
+VALUES
+(15, 'sales_invoice_cost', 'تكلفة بضاعة مباعة', 'COGS Journal Entry for Sales Invoice', NOW(), NOW());
