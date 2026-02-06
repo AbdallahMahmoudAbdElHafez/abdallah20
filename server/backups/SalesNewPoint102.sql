b6830cc3da7fc71cc0287685241ebda32bac877a
AbdallahMahmoudAbdElHafez <abdallah.mahmoud.te@gmail.com>
2026-02-06 20:02:59 +0200

    6.2.2026

--- server/backups/SalesNewPoint102.sql
+++ server/backups/SalesNewPoint102.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `accounting_settings`
 --
-
+use nurivina_erp;
 DROP TABLE IF EXISTS `accounting_settings`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
